ed69bfeba4fd2999d0542ec9598a1c8bbbf654f0
Erik Flores <erik.flores@colorado.edu>
2020-03-16 19:46:19 -0600

    bugs

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const api = {
     key: "b7d11377cf13815e03717205de88a50c",
-    baseurl: "https:api.openweathermap.org/data/2.5/"
+    base: "https://api.openweathermap.org/data/2.5/"
 }
 
 const searchbox =document.querySelector('.search-box')
@@ -9,12 +9,11 @@ searchbox.addEventListener('keypress', setQuery)
 function setQuery(evt) {
     if (evt.keyCode == 13) {
         getResults(searchbox.value)
-        console.log(searchbox.value)
     }
 }
 
 function getResults (query) {
-    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
     .then(weather => {
         return weather.json()
     }).then(displayResults)
@@ -29,13 +28,13 @@ function displayResults (weather) {
     date.innerText = dateBuilder(now);
   
     let temp = document.querySelector('.current .temp');
-    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°F</span>`;
   
     let weather_el = document.querySelector('.current .weather');
     weather_el.innerText = weather.weather[0].main;
   
     let hilow = document.querySelector('.hi-low');
-    hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`; 
+    hilow.innerText = `${Math.round(weather.main.temp_min)}°F / ${Math.round(weather.main.temp_max)}°c`; 
 }
 
 function dateBuilder (d) {
